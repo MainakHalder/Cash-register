bd7dc1d7d9ccd67db68119ea333d58f6bc82978d
MainakHalder <76676017+MainakHalder@users.noreply.github.com>
2022-07-23 23:58:20 +0530

    fix- string to int

--- index.js
+++ index.js
@@ -6,9 +6,11 @@ const noOfNotes = document.querySelectorAll(".no-of-notes");
 const availableNotes = [2000, 500, 100, 20, 10, 5, 1];
 checkButton.addEventListener("click" ,function validateBillAndCashAmount() {
     hideMessage();
-   if(billAmount.value > 0){
-    if(cashGiven.value >= billAmount.value){
-        const amountToBeReturned = cashGiven.value - billAmount.value;
+    const amount = Number(billAmount.value);
+    const cash = Number(cashGiven.value);
+   if(amount > 0){
+    if(cash >= amount){
+        const amountToBeReturned = cash - amount;
         calculateChange(amountToBeReturned);
     }
     else {
